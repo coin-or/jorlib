2022483c08111d2318f29d75356cbe916c6ee987
Joris Kinable <j.kinable@gmail.com>
2016-07-05 12:33:34 -0400

    Updated INSTALL instructions

---
+++ jorlib-demo/src/main/java/org/jorlib/demo/frameworks/columnGeneration/graphColoringBAP/cg/GCPColGen.java
@@ -0,0 +1,9 @@
+package org.jorlib.demo.frameworks.columnGeneration.graphColoringBAP.cg;
+
+import org.jorlib.frameworks.columnGeneration.colgenMain.ColGen;
+
+/**
+ * Created by jkinable on 7/2/16.
+ */
+public class GCPColGen extends ColGen<>{
+}
