9648b3fef626ae7fe25dd819b930f8fb20f6bef4
Joris Kinable <j.kinable@gmail.com>
2016-07-24 23:13:27 -0400

    Removed temporary class

--- jorlib-demo/src/main/java/org/jorlib/demo/frameworks/columnGeneration/graphColoringBAP/cg/GCPColGen.java
+++
@@ -1,9 +0,0 @@
-package org.jorlib.demo.frameworks.columnGeneration.graphColoringBAP.cg;
-
-import org.jorlib.frameworks.columnGeneration.colgenMain.ColGen;
-
-/**
- * Created by jkinable on 7/2/16.
- */
-public class GCPColGen extends ColGen<>{
-}
