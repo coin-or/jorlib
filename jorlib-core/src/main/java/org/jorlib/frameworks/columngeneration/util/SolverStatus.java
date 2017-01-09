197a86513a7036b8578458fb2183827775a4164a
Rowan <rowtricker@gmail.com>
2017-01-09 11:06:43 +0100

    Added common class header

--- jorlib-core/src/main/java/org/jorlib/frameworks/columngeneration/util/SolverStatus.java
+++ jorlib-core/src/main/java/org/jorlib/frameworks/columngeneration/util/SolverStatus.java
@@ -1,3 +1,15 @@
+/* ==========================================
+ * jORLib : Java Operations Research Library
+ * ==========================================
+ *
+ * Project Info:  http://www.coin-or.org/projects/jORLib.xml
+ * Project Creator:  Joris Kinable (https://github.com/jkinable)
+ *
+ * (C) Copyright 2017-2017, by Rowan Hoogervorst and Contributors.
+ *
+ * This program and the accompanying materials are licensed under LGPLv2.1
+ * as published by the Free Software Foundation.
+ */
 package org.jorlib.frameworks.columngeneration.util;
 
 /**
